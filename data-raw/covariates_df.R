0ebf1db6d4bfde169a3b65917d386d62563bc12a
Seth Caldwell <caldwellst@gmail.com>
2021-01-04 16:34:45 +0000

    Fix `covariates_df`

--- data-raw/covariates_df.R
+++ data-raw/covariates_df.R
@@ -3,12 +3,9 @@ library(dplyr)
 
 covariates_df <- readr::read_csv("data-raw/covariates_df.csv") %>%
   select(iso3,
-            year,
-            region,
-            sdi,
-            cdr,
-            e0,
-            q5,
-            q1)
+         year,
+         region,
+         sdi,
+         e0)
 
 usethis::use_data(covariates_df, overwrite = TRUE)
