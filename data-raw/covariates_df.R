a97f1a9c8580c40311faad6b7e2c280d42d504cd
Seth Caldwell <caldwellst@gmail.com>
2021-02-09 10:33:16 +0000

    Remove old covariates from data_raw

--- data-raw/covariates_df.R
+++ data-raw/covariates_df.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 load("data-raw/functions_covariates.RDa")
 
-functions_covariates$covariates.df %>%
+covariates_df <- functions_covariates$covariates.df %>%
   as_tibble() %>%
   transmute(iso3,
             year,
@@ -11,18 +11,6 @@ functions_covariates$covariates.df %>%
             region,
             sdi,
             sdi_scaled = scales::rescale(sdi, c(0,1)),
-            e0,
-            e0_scaled = scales::rescale(e0, c(0, 1)))
-
-
-covariates_df <- readr::read_csv("data-raw/covariates_df.csv") %>%
-  transmute(iso3,
-            year,
-            year_n = year - 1999,
-            region,
-            sdi,
-            sdi_scaled = scales::rescale(sdi, c(0,1)),
-            sdi_scaled2 = (sdi - min(sdi, na.rm = T)) / (max(sdi, na.rm = T) - min(sdi, na.rm = T)),
             e0,
             e0_scaled = scales::rescale(e0, c(0, 1)))
 
