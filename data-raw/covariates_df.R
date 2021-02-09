92e209611811b751953be60b432c01c94ce46567
Seth Caldwell <caldwellst@gmail.com>
2021-02-09 10:28:03 +0000

    Update covariates_df, close #11

--- data-raw/covariates_df.R
+++ data-raw/covariates_df.R
@@ -1,6 +1,20 @@
 ## code to prepare `covariates_df` dataset goes here
 library(dplyr)
 
+load("data-raw/functions_covariates.RDa")
+
+functions_covariates$covariates.df %>%
+  as_tibble() %>%
+  transmute(iso3,
+            year,
+            year_n = year - 1999,
+            region,
+            sdi,
+            sdi_scaled = scales::rescale(sdi, c(0,1)),
+            e0,
+            e0_scaled = scales::rescale(e0, c(0, 1)))
+
+
 covariates_df <- readr::read_csv("data-raw/covariates_df.csv") %>%
   transmute(iso3,
             year,
